3b2e494454c2778f39afbd76fbec2f6a42ac0c26
Kathryn Reagan <kathryn.m.reagan@gmail.com>
2016-05-27 17:07:42 -0500

    completed exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        float sumOfAreas = 0;
+        for(Rectangle rectangle : rectangles){
+            sumOfAreas += rectangle.area();
+        }
+
+        return sumOfAreas / rectangles.length;
+
     }
 }
