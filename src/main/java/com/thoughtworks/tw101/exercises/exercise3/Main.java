14f0bed6237280bc2b6369c9dbdec349a7d53c85
Kathryn Reagan <kathryn.m.reagan@gmail.com>
2016-05-27 11:19:19 -0500

    completes exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
@@ -2,8 +2,8 @@ package com.thoughtworks.tw101.exercises.exercise3;
 
 public class Main {
 
-//    Exercise #3: Create a function 'of' in the class OddSum that returns the sum of all odd integers between a starting
-//    and ending integer value.
+//    Exercise #3: Create a function 'of' in the class OddSum that returns the sum of all
+//    odd integers between a starting and ending integer value.
 
     public static void main(String[] args) {
         OddSum oddSum = new OddSum();
