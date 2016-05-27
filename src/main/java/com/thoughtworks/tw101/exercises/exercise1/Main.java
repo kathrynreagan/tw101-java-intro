a43af74ce9f2ca96eefa09cdd4459434bccfa69c
Kathryn Reagan <kathryn.m.reagan@gmail.com>
2016-05-27 09:29:58 -0500

    completed exercise 1

---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.tw101.exercises.exercise1;
+
+// Exercise #1: Print all odd numbers from 1 to 100 and their sum.
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i <= 100; i+=2){
+            System.out.print(i + " ");
+            sum += i;
+        }
+        System.out.println("\nSum of all odd numbers from 1 to 100: " + sum);
+    }
+}
