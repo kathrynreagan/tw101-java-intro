b9ab1aab4f400159ce07d94076e11889fa6f3bdb
Kathryn Reagan <kathryn.m.reagan@gmail.com>
2016-06-01 21:45:44 -0500

    completed ex 9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,65 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Node {
-    private String name;
+
+    /* private inner class to represent nodes that contain strings
+     * in order to implement a binary tree of strings     */
+    private class StringNode {
+        String name;
+        StringNode left;
+        StringNode right;
+
+        StringNode(String name){
+            this.name = name;
+        }
+    }
+
+    private StringNode root;
 
     public Node(String name) {
-        this.name = name;
+        this.root = new StringNode(name);
     }
 
     public void add(String nameOfNewNode) {
+        root = recursiveAdd(root, nameOfNewNode);
     }
 
+    private StringNode recursiveAdd(StringNode node, String nameOfNewNode) {
+        if(node == null){
+            node = new StringNode(nameOfNewNode);
+        }
+        else {
+            int comparison = nameOfNewNode.compareTo(node.name);
+            if(comparison <= 0){
+               node.left = recursiveAdd(node.left, nameOfNewNode);
+            }
+            else {
+                node.right = recursiveAdd(node.right, nameOfNewNode);
+            }
+        }
+        return node;
+    }
+
+
     public List<String> names() {
-        return null;
+        List<String> names = new LinkedList<String>();
+
+        recursiveNames(names, root);
+
+        return names;
+    }
+
+    private void recursiveNames(List<String> names, StringNode node) {
+        if(node == null){
+            // do nothing
+        }
+        else {
+            recursiveNames(names, node.left);
+            names.add(node.name);
+            recursiveNames(names, node.right);
+        }
     }
 }
